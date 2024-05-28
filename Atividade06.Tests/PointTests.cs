e497ee209b01a7cf7f6fc51aa32a4ee23f7f8041
Joao Pedro de Gois Pinto <jpcic07@gmail.com>
2024-05-28 10:30:03 -0300

    adding pointTests

--- Atividade06.Tests/PointTests.cs
+++ Atividade06.Tests/PointTests.cs
@@ -6,12 +6,34 @@ public class PointTests
     public void DistanceTo_WithNull_ReturnsFalse()
     {
         // Arrange
-            Point point1 = new Point(1, 2);
+        Point point1 = new Point(1, 2);
 
-            // Act
-            double distance = point1.DistanceTo(point1);
+        // Act
+        double distance = point1.DistanceTo(point1);
 
-            // Assert
-            Assert.Equal(0, distance);
+        // Assert
+        Assert.Equal(0, distance);
+    }
+    [Fact]
+    public void DistanceTo_DifferentPoints_ShouldReturnCorrectDistance()
+    {
+        // Arrange
+        Point point1 = new Point(1, 2);
+        Point point2 = new Point(4, 6);
+
+        // Act
+        double distance = point1.DistanceTo(point2);
+
+        // Assert
+        Assert.Equal(5, distance);
+    }
+    [Fact]
+    public void DistanceTo_NullArgument_ShouldThrowArgumentNullException()
+    {
+        // Arrange
+        Point point1 = new Point(1, 2);
+
+        // Act & Assert
+        Assert.Throws<System.ArgumentNullException>(() => point1.DistanceTo(null));
     }
 }
