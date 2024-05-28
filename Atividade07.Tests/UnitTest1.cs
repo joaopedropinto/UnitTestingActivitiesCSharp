1563de3a1f8dd056d28dcc9a1cd1cdf31b93631c
Joao Pedro de Gois Pinto <jpcic07@gmail.com>
2024-05-28 10:11:22 -0300

    adding branch

---
+++ Atividade07.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Atividade07.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Atividade09.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Atividade09.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
